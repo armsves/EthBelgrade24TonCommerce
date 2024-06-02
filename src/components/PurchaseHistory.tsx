be57cbae3a65139870a20441b46bdf1788fb82de
armsves <armsves@gmail.com>
2024-06-02 09:39:20 +0200

    testing live final final?

--- src/components/PurchaseHistory.tsx
+++ src/components/PurchaseHistory.tsx
@@ -20,7 +20,7 @@ export function PurchaseHistory() {
 
     const fetchPurchases = async (): Promise<Purchase[]> => {
         try {
-            const response = await fetch(`${process.env.NGROK_URL}/purchase-history/${wallet}`, {
+            const response = await fetch(`https://0726-87-116-161-246.ngrok-free.app/purchase-history/${wallet}`, {
                 method: "GET",
                 headers: new Headers({
                     "ngrok-skip-browser-warning": "69420",
